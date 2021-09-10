372d78d7a6d4863ff4904ea491a969bc0781a3f4
student <mpatankar06@gmail.com>
2021-09-10 11:37:29 -0400

    chocolate yum

--- src/com/company/YummyFood.java
+++ src/com/company/YummyFood.java
@@ -6,12 +6,14 @@ public class YummyFood {
         iceCream();
     }
 
-    public static void chocolate() {
-        for (int i=0; i<20; i++) {
+    public static void chocolate() throws Exception {
+        for (int i = 0; i < 20; i++) {
             if (i % 2 == 0) {
                 System.out.println("\u001B[35m" + "Chocolate");
             } else if (i % 2 != 0) {
                 System.out.println("\u001B[32m" + "Chocolate");
+            } else {
+                throw new Exception();
             }
         }
 
@@ -19,7 +21,11 @@ public class YummyFood {
 
     public  static  void iceCream() {
         System.out.println("\u001B[34m" + "Chunky Chocolate Pudding");
-        chocolate();
+        try {
+            chocolate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("\u001B[31m" + "that was yummy");
     }
 }
